3d060b7f23bd8216c9abc993a6293c27d986548d
JorgeDuarte2701 <55166218+JorgeDuarte2701@users.noreply.github.com>
2022-02-19 09:44:21 -0600

    Add files via upload

--- Persona.cpp
+++ Persona.cpp
@@ -8,28 +8,7 @@ main(){
 	double cui;
 	int telefono;
 	
-	cout<<"Ingresar Nit :";
-	cin>>nit;	
-	cout<<"Ingresar Cui :";
-	cin>>nit;
-	cout<<"Ingresar Nombres :";
-	cin>>nombres;
-	cout<<"Ingresar Apellidos :";
-	cin>>apellidos;
-	cout<<"Ingresar Direccion :";
-	cin>>direccion;
-	cout<<"Ingresar Telefono :";
-	cin>>telefono;
-	//instancia de un objeto
-	Cliente obj = Cliente(nombres,apellidos,direccion,telefono,fecha_N,nit,cui);
-	obj.mostrar();
-	cout<<"Ingresar Nit :";
-	cin>>nit;
-	obj.setNit(nit);
-	
-	cout<<"Ingresar Cui :";
-	cin>>cui;
-	obj.setNit(cui);
+
 	//atributos
 	protected : string nombres,apellidos, direccion,fecha_n;
 		int telefono;
@@ -48,5 +27,36 @@ main(){
 	//metodo
 
 	void mostrar();
+	void agregar(){
+			string nit, nombres,apellidos,direccion,fecha_N;
+	double cui;
+	int telefono;
+	
+	cout<<"Ingresar Nit :";
+	cin>>nit;	
+	cout<<"Ingresar Cui :";
+	cin>>nit;
+	cout<<"Ingresar Nombres :";
+	cin>>nombres;
+	cout<<"Ingresar Apellidos :";
+	cin>>apellidos;
+	cout<<"Ingresar Direccion :";
+	cin>>direccion;
+	cout<<"Ingresar Telefono :";
+	cin>>telefono;
+	cout<<"Ingresar Fecha de naciemiento :";
+	cin>>fecha_N;
+	//instancia de un objeto
+	Cliente obj = Cliente(nombres,apellidos,direccion,telefono,fecha_N,nit,cui);
+	obj.mostrar();
+	cout<<"Ingresar Nit :";
+	cin>>nit;
+	obj.setNit(nit);
+	
+	cout<<"Ingresar Cui :";
+	cin>>cui;
+	obj.setNit(cui);
+}
+	}
 	
 };
